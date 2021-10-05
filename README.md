fbb42cca6797b34d744eaddc88b2c3dc380e0cac
Micheal Jones <micheal@mcjones.ca>
2021-10-05 12:09:02 -0600

    PowerDNS 4 changes

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Really simple PowerDNS module that expects a site module to pass in settings pulled from Hiera as k/v store and writes it to the PowerDNS configs.
 
+If you're looking for a more featureful and better tested module check out: https://github.com/sensson/puppet-powerdns
+
 Example use:
 
 Hiera:
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,6 @@
 # == Class: powerdns::install
 #
-# Installs PowerDNS using OS package manager
+# Installs PowerDNS 4.4 using repo.powerdns.com
 #
 # === Parameters
 #
@@ -8,6 +8,28 @@
 class powerdns::install (
   $package_ensure = latest,
 ) {
+
+  apt::key { 'powerdns':
+    ensure => present,
+    id     => '9FAAA5577E8FCF62093D036C1B0C6205FD380FBB',
+    source => 'https://repo.powerdns.com/FD380FBB-pub.asc',
+  }
+
+  apt::source { 'repo.powerdns.com':
+    ensure => present,
+    location => 'http://repo.powerdns.com/ubuntu',
+    repos => 'main',
+    release => 'xenial-auth-44',
+    architecture => 'amd64',
+    require      => Apt::Key['powerdns'],
+  }
+
+  apt::pin { 'powerdns':
+    priority => 600,
+    packages => 'pdns-*',
+    origin   => 'repo.powerdns.com',
+    require  => Apt::Source['powerdns'],
+  }
 
   $default_package_name = $::osfamily ? {
     'Debian' => 'pdns-server',
